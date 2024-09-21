933e54c1e0c7dd85f5400f203c015d7d3e1b2b4d
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-09-21 22:20:54 +0545

    bubbleEffect updateed

--- src/app/uiEffects/BubbleEffect.tsx
+++ src/app/uiEffects/BubbleEffect.tsx
@@ -4,7 +4,7 @@ import "./BubbleEffect.css";
 const BubbleEffect = () => {
   return (
     <div className="wrapper">
-      {Array.from({ length: 20 }).map((_, index) => (
+      {Array.from({ length: 7 }).map((_, index) => (
         <div key={index}>
           <span className="dot"></span>
         </div>
