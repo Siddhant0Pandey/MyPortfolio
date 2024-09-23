2d2c14940be717d9fd49ef9dccd9e732b0e676d6
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-09-23 23:06:10 +0545

    portfolio page updated

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import { projectItems } from "./projects/project";
+import Link from "next/link";
+import ProjectDescription from "@/app/uiEffects/ProjectDescription";
 
 function Portfolio() {
   return (
@@ -11,19 +13,34 @@ function Portfolio() {
           My Portfolio
         </div>
         {/* Apply flex-col for small screens and grid for larger screens */}
-        <div className="flex flex-col sm:grid sm:grid-rows-6 sm:grid-cols-6 h-[80vh] w-full border border-white p-4 gap-4">
-          <div className="item-1 row-start-1 row-end-3 col-start-1 col-end-3 bg-red-600 hover:p-6 hover:row-end-6 hover:col-end-6 hover:z-50 transition-[grid-template-rows,grid-template-columns] duration-2000 ease-in-out rounded-md">
+        <div className="flex flex-col sm:grid sm:grid-rows-6 sm:grid-cols-6 h-[80vh] w-full  p-4 gap-4">
+          <div className="item-1 row-start-1 row-end-3 col-start-1 col-end-3 bg-accent   hover:row-end-6 hover:col-end-6 hover:z-50 transition-[grid-template-rows,grid-template-columns] duration-2000 ease-in-out rounded-md overflow-hidden">
             <div>
               {projectItems.map((detail) => {
                 return (
-                  <div key={detail.id}>
+                  <div key={detail.id} className="relative  project_style">
                     {detail.id == 1 ? (
-                      <div className="flex flex-col items-center p-2">
-                        {" "}
-                        <Image src={detail.image} alt={detail.name} />
-                        <h1>{detail.name}</h1>
-                        <p>{detail.description}</p>
-                      </div>
+                      <Link href="./google.com" target="_blank" className="">
+                        <Image
+                          src={detail.image.src}
+                          alt={detail.name}
+                          width={detail.image.width}
+                          height={detail.image.height}
+                          objectFit="cover"
+                          className=" opacity-90 -z-10 rounded-md "
+                        />
+                        <div className="absolute top-[40%] left-0 text-accent bg-white w-full  project_style_title">
+                          <h1 className="text-center hover:text-2xl">
+                            {detail.name}
+                          </h1>
+                        </div>
+                        <div className="absolute w-full h-full bg-black top-[0%] left-0  opacity-0  project_style_description">
+                          <ProjectDescription
+                            description={detail.description}
+                            name={detail.name}
+                          />
+                        </div>
+                      </Link>
                     ) : (
                       ""
                     )}
@@ -32,26 +49,250 @@ function Portfolio() {
               })}
             </div>
           </div>
-          <div className="row-start-1 row-end-3 col-start-3 col-end-6 bg-gray-600 hover:row-end-6 hover:col-start-6 hover:col-end-1 hover:z-50 rounded-md">
-            2
+          <div className="row-start-1 overflow-hidden row-end-3 col-start-3 col-end-6 bg-gray-600 hover:row-end-6 hover:col-start-6 hover:col-end-1 hover:z-50 rounded-md">
+            <div>
+              {projectItems.map((detail) => {
+                return (
+                  <div key={detail.id} className="relative  project_style">
+                    {detail.id == 2 ? (
+                      <Link href="./google.com" target="_blank" className="">
+                        <Image
+                          src={detail.image.src}
+                          alt={detail.name}
+                          width={detail.image.width}
+                          height={detail.image.height}
+                          objectFit="cover"
+                          className=" opacity-90 -z-10 rounded-md "
+                        />
+                        <div className="absolute top-[40%] left-0 text-accent bg-white w-full  project_style_title">
+                          <h1 className="text-center hover:text-2xl">
+                            {detail.name}
+                          </h1>
+                        </div>
+                        <div className="absolute w-full h-full bg-black top-[0%] left-0  opacity-0  project_style_description">
+                          <ProjectDescription
+                            description={detail.description}
+                            name={detail.name}
+                          />
+                        </div>
+                      </Link>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="row-start-1 row-end-6 col-start-6 col-end-7 bg-cyan-600 hover:col-start-7 hover:col-end-2 hover:z-50 rounded-md">
-            3
+          <div className="overflow-hidden row-start-1 row-end-6 col-start-6 col-end-7 bg-cyan-600 hover:col-start-7 hover:col-end-2 hover:z-50 rounded-md">
+            <div>
+              {projectItems.map((detail) => {
+                return (
+                  <div key={detail.id} className="relative  project_style">
+                    {detail.id == 3 ? (
+                      <Link href="./google.com" target="_blank" className="">
+                        <Image
+                          src={detail.image.src}
+                          alt={detail.name}
+                          width={detail.image.width}
+                          height={1000}
+                          objectFit="cover"
+                          className=" opacity-90 -z-10 rounded-md h-full"
+                        />
+                        <div className="absolute top-[40%] left-0 text-accent bg-white w-full project_style_title">
+                          <h1 className="text-center hover:text-2xl">
+                            {detail.name}
+                          </h1>
+                        </div>
+                        <div className="absolute w-full h-full bg-black top-[0%] left-0  opacity-0  project_style_description">
+                          <ProjectDescription
+                            description={detail.description}
+                            name={detail.name}
+                          />
+                        </div>
+                      </Link>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="row-start-6 row-end-7 col-start-3 col-end-7 bg-slate-700 hover:row-end-2 hover:col-start-7 hover:row-start-7 hover:col-end-2 hover:z-50 rounded-md">
-            4
+          <div className="overflow-hidden row-start-6  row-end-7 col-start-3 col-end-7 bg-slate-700 hover:row-end-2 hover:col-start-7 hover:row-start-7 hover:col-end-2 hover:z-50 rounded-md">
+            <div>
+              {projectItems.map((detail) => {
+                return (
+                  <div key={detail.id} className="relative  project_style">
+                    {detail.id == 4 ? (
+                      <Link href="./google.com" target="_blank" className="">
+                        <Image
+                          src={detail.image.src}
+                          alt={detail.name}
+                          width={detail.image.width}
+                          height={detail.image.height}
+                          objectFit="contain"
+                          className=" opacity-90 -z-10 rounded-md "
+                        />
+                        <div className="absolute top-[40%] left-0 text-accent bg-white w-full  project_style_title">
+                          <h1 className="text-center hover:text-2xl">
+                            {detail.name}
+                          </h1>
+                        </div>
+                        <div className="absolute w-full h-full bg-black top-[0%] left-0  opacity-0  project_style_description">
+                          <ProjectDescription
+                            description={detail.description}
+                            name={detail.name}
+                          />
+                        </div>
+                      </Link>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="row-start-3 row-end-6 col-start-2 col-end-5 bg-green-700 hover:row-start-6 hover:row-end-1 hover:col-end-7 hover:z-50 rounded-md">
-            5
+          <div className="overflow-hidden row-start-3 row-end-6 col-start-2 col-end-5 bg-green-700 hover:row-start-6 hover:row-end-1 hover:col-end-7 hover:z-50 rounded-md">
+            <div>
+              {projectItems.map((detail) => {
+                return (
+                  <div key={detail.id} className="relative  project_style">
+                    {detail.id == 5 ? (
+                      <Link href="./google.com" target="_blank" className="">
+                        <Image
+                          src={detail.image.src}
+                          alt={detail.name}
+                          width={detail.image.width}
+                          height={detail.image.height}
+                          objectFit="contain"
+                          className=" opacity-90 -z-10 rounded-md "
+                        />
+                        <div className="absolute top-[40%] left-0 text-accent bg-white w-full project_style_title">
+                          <h1 className="text-center hover:text-2xl">
+                            {detail.name}
+                          </h1>
+                        </div>
+                        <div className="absolute w-full h-full bg-black top-[0%] left-0  opacity-0  project_style_description">
+                          <ProjectDescription
+                            description={detail.description}
+                            name={detail.name}
+                          />
+                        </div>
+                      </Link>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="row-start-3 row-end-6 col-start-5 col-end-6 bg-orange-600 hover:row-start-6 hover:row-end-1 hover:col-start-6 hover:col-end-1 hover:z-50 rounded-md">
-            6
+          <div className="overflow-hidden row-start-3 row-end-6 col-start-5 col-end-6 bg-orange-600 hover:row-start-6 hover:row-end-1 hover:col-start-6 hover:col-end-1 hover:z-50 rounded-md">
+            <div>
+              {projectItems.map((detail) => {
+                return (
+                  <div key={detail.id} className="relative  project_style">
+                    {detail.id == 6 ? (
+                      <Link href="./google.com" target="_blank" className="">
+                        <Image
+                          src={detail.image.src}
+                          alt={detail.name}
+                          width={detail.image.width}
+                          height={detail.image.height}
+                          objectFit="contain"
+                          className=" opacity-90 -z-10 rounded-md "
+                        />
+                        <div className="absolute top-[40%] left-0 text-accent bg-white w-full  project_style_title">
+                          <h1 className="text-center hover:text-2xl">
+                            {detail.name}
+                          </h1>
+                        </div>
+                        <div className="absolute w-full h-full bg-black top-[0%] left-0  opacity-0  project_style_description">
+                          <ProjectDescription
+                            description={detail.description}
+                            name={detail.name}
+                          />
+                        </div>
+                      </Link>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="row-start-3 row-end-6 col-start-1 col-end-2 bg-zinc-600 hover:row-start-6 hover:row-end-1 hover:col-end-6 hover:z-50 rounded-md">
-            7
+          <div className="overflow-hidden row-start-3 row-end-6 col-start-1 col-end-2 bg-zinc-600 hover:row-start-6 hover:row-end-1 hover:col-end-6 hover:z-50 rounded-md">
+            <div>
+              {projectItems.map((detail) => {
+                return (
+                  <div key={detail.id} className="relative  project_style">
+                    {detail.id == 7 ? (
+                      <Link href="./google.com" target="_blank" className="">
+                        <Image
+                          src={detail.image.src}
+                          alt={detail.name}
+                          width={detail.image.width}
+                          height={detail.image.height}
+                          objectFit="contain"
+                          className=" opacity-90 -z-10 rounded-md "
+                        />
+                        <div className="absolute top-[40%] left-0 text-accent bg-white w-full  project_style_title">
+                          <h1 className="text-center hover:text-2xl">
+                            {detail.name}
+                          </h1>
+                        </div>
+                        <div className="absolute w-full h-full bg-black top-[0%] left-0  opacity-0  project_style_description">
+                          <ProjectDescription
+                            description={detail.description}
+                            name={detail.name}
+                          />
+                        </div>
+                      </Link>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="row-start-6 row-end-7 col-start-1 col-end-3 bg-emerald-700 hover:row-start-7 hover:row-end-2 hover:col-end-6 hover:z-50 rounded-md delay-500">
-            8
+          <div className="overflow-hidden row-start-6 row-end-7 col-start-1 col-end-3 bg-emerald-700 hover:row-start-7 hover:row-end-2 hover:col-end-6 hover:z-50 rounded-md delay-500">
+            <div>
+              {projectItems.map((detail) => {
+                return (
+                  <div key={detail.id} className="relative  project_style">
+                    {detail.id == 8 ? (
+                      <Link href="./google.com" target="_blank" className="">
+                        <Image
+                          src={detail.image.src}
+                          alt={detail.name}
+                          width={detail.image.width}
+                          height={detail.image.height}
+                          objectFit="contain"
+                          className=" opacity-90 -z-10 rounded-md "
+                        />
+                        <div className="absolute top-[40%] left-0 text-accent bg-white w-full z-50 project_style_title">
+                          <h1 className="text-center hover:text-2xl">
+                            {detail.name}
+                          </h1>
+                        </div>
+                        <div className="absolute w-full h-full bg-black top-[0%] left-0  opacity-0  project_style_description">
+                          <ProjectDescription
+                            description={detail.description}
+                            name={detail.name}
+                          />
+                        </div>
+                      </Link>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
--- src/app/components/projects/project.ts
+++ src/app/components/projects/project.ts
@@ -2,60 +2,112 @@ import martheoMovieImg from "../../../../public/images/martheomovies.jpg";
 import tradingCourseImg from "../../../../public/images/Stock-Trading-Courses.png";
 import pokeSearchImg from "../../../../public/images/pokesearch.jpeg";
 
-export const projectItems = [
+interface ProjectItemProps {
+  id: number;
+  name: string;
+  image: {
+    src: string;
+    alt?: string;
+    width: number;
+    height: number;
+  };
+  description: string;
+}
+
+export const projectItems: ProjectItemProps[] = [
   {
     id: 1,
     name: "Martheo Movies",
-    image: { martheoMovieImg },
+    image: {
+      src: martheoMovieImg.src,
+      alt: "Martheo Movies",
+      width: martheoMovieImg.width,
+      height: martheoMovieImg.height,
+    },
     description:
       "A recreation of the Disney+ streaming service. The clone includes a dynamic homepage, and a detailed view for each title.",
   },
   {
     id: 2,
     name: "TradeAcademy",
-    image: { tradingCourseImg },
+    image: {
+      src: tradingCourseImg.src,
+      alt: "Martheo Movies",
+      width: tradingCourseImg.width,
+      height: tradingCourseImg.height,
+    },
     description:
       "This site showcases a UI designed to explore and demonstrate the capabilities of the Next.js",
   },
   {
     id: 3,
     name: "PokeSearch",
-    image: { pokeSearchImg },
+    image: {
+      src: pokeSearchImg.src,
+      alt: "Martheo Movies",
+      width: pokeSearchImg.width,
+      height: pokeSearchImg.height,
+    },
     description:
       "This site contains the name, image and type of the pokemon, it includes the search feature to search the pokemons. up",
   },
   {
     id: 4,
     name: "ClassManager",
-    image: { pokeSearchImg },
+    image: {
+      src: martheoMovieImg.src,
+      alt: "Martheo Movies",
+      width: martheoMovieImg.width,
+      height: martheoMovieImg.height,
+    },
     description:
       "A student information system that supports adding and deleting students, managing grades, attendance, and more for schools and colleges.",
   },
   {
     id: 5,
     name: "ClassManager",
-    image: { pokeSearchImg },
+    image: {
+      src: martheoMovieImg.src,
+      alt: "Martheo Movies",
+      width: martheoMovieImg.width,
+      height: martheoMovieImg.height,
+    },
     description:
       "A student information system that supports adding and deleting students, managing grades, attendance, and more for schools and colleges.",
   },
   {
     id: 6,
     name: "PokeSearch",
-    image: { pokeSearchImg },
+    image: {
+      src: pokeSearchImg.src,
+      alt: "Martheo Movies",
+      width: pokeSearchImg.width,
+      height: pokeSearchImg.height,
+    },
     description:
       "This site contains the name, image and type of the pokemon, it includes the search feature to search the pokemons. up",
   },
   {
     id: 7,
     name: "TradeAcademy",
-    image: { tradingCourseImg },
+    image: {
+      src: tradingCourseImg.src,
+      alt: "Martheo Movies",
+      width: tradingCourseImg.width,
+      height: tradingCourseImg.height,
+    },
     description:
       "This site showcases a UI designed to explore and demonstrate the capabilities of the Next.js",
   },
   {
     id: 8,
     name: "Martheo Movies",
-    image: { martheoMovieImg },
+    image: {
+      src: martheoMovieImg.src,
+      alt: "Martheo Movies",
+      width: martheoMovieImg.width,
+      height: martheoMovieImg.height,
+    },
     description:
       "A recreation of the Disney+ streaming service. The clone includes a dynamic homepage, and a detailed view for each title.",
   },
